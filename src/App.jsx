9cae1796823e3b9547cc6bbccef5ac391cdd7c12
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-06-13 21:04:09 +0500

    useState Full CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import React, {useState} from 'react';
-
+import React from 'react';
+import CRUD from './CRUD';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState();
-
-
-  const Increment = () => {
-    setCount(prevstate => prevstate + 1);
-  }
-
-  const Decrement = () => {
-    setCount(prevstate => prevstate - 1);
-  }
-
-  const Reset = () => {
-    setCount(0);
-  }
-
-  const onNameChange = (e) => {
-    setName(e.target.value)
-  }
-
   return (
     <div>
-      <h1>Name: {name}</h1>
-      <input onChange={onNameChange} name='name' type="text" placeholder='name' />
-      <hr />
-      <button onClick={Increment} >increment</button>
-      <button onClick={Decrement} >decrement</button>
-      <button onClick={Reset} >reset</button>
-      <h1>Counter: {count}</h1>
+      <CRUD />
     </div>
   )
 }
 
 export default App
-
